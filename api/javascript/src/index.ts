551d3f78500477e32593a9ebdbbb4732314849c0
Swa-ne <spaulbautista@gmail.com>
2025-01-16 23:33:30 +0800

    Add common middleware dependencies

--- api/javascript/src/index.ts
+++ api/javascript/src/index.ts
@@ -1,10 +1,27 @@
 import express, { Request, Response } from 'express';
+import cors from 'cors';
+import bodyParser from "body-parser";
+import cookieParser from 'cookie-parser';
 
 const app = express();
 
 const port = 3000;
 
+
+app.set('trust proxy', 1);
+app.use(
+    cors({
+        origin: process.env.CLIENT_URL,
+        credentials: true,
+    })
+);
 app.use(express.json());
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    }),
+);
+app.use(cookieParser());
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello from your Node.js Express server!');
