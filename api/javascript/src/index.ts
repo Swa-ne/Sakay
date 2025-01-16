d81a4471d6859aeee4e341ba2963e850b988212e
Swa-ne <spaulbautista@gmail.com>
2025-01-16 23:20:59 +0800

    Initialized TypeScript with Express API

---
+++ api/javascript/src/index.ts
@@ -0,0 +1,15 @@
+import express, { Request, Response } from 'express';
+
+const app = express();
+
+const port = 3000;
+
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello from your Node.js Express server!');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
