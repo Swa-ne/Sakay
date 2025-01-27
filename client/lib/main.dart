5d69172cd3ef5680c21970142d90823aa5b137c3
Swa-ne <spaulbautista@gmail.com>
2025-01-27 21:03:19 +0800

    Integrated Mapbox access token and dotenv setup in main.dart for environment-specific configuration.

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:mapbox_maps_flutter/mapbox_maps_flutter.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:sakay_app/presentation/screens/intro/splashscreen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await dotenv.load(fileName: ".env");
+  String ACCESS_TOKEN = "${dotenv.env['ACCESS_TOKEN']}";
+  MapboxOptions.setAccessToken(ACCESS_TOKEN);
+
   runApp(const MyApp());
 }
 
