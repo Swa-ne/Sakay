005e7a51f5e6d3efc5ba448788b480863369c095
Swa-ne <spaulbautista@gmail.com>
2025-01-30 20:22:02 +0800

    Added 'track-me' event to log user location details upon receiving location data from the client.

--- api/javascript/src/socket.ts
+++ api/javascript/src/socket.ts
@@ -22,12 +22,13 @@ const io = new Server(server, {
 
 io.on("connection", async (socket) => {
     socketAuthenticate(io);
-
     if (socket.newAccessToken) {
         socket.emit('newAccessToken', { access_token: socket.newAccessToken });
     }
     addUserToRedisController(socket.id, socket.user);
-
+    socket.on('track-me', async (location) => {
+        console.log(`location of user ${socket.user?.full_name}: Longitude is ${location.longitude}, Latitude is ${location.latitude}, Speed is ${location.speed}`)
+    })
     socket.on("disconnect", async () => {
         removeUserToRedisController(socket.id, socket.user)
         console.log(`disconnected ${socket.id}`)
