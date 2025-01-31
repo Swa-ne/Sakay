f64cd32b280703dcb21dc945d892df3b7f2df3f6
Jaspherr <taniajaspher@gmail.com>
2025-01-31 12:34:40 +0800

    Admin First Interative Pages

---
+++ client/lib/presentation/screens/admin/map.dart
@@ -0,0 +1,154 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:sakay_app/common/widgets/map.dart';
+import 'package:sakay_app/presentation/screens/admin/surveillance.dart';
+
+class Map extends StatefulWidget {
+  const Map({super.key});
+
+  @override
+  _MapState createState() => _MapState();
+}
+
+class _MapState extends State<Map> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: Drawer(
+        child: Container(
+          color: const Color(0xFF00A3FF),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 50),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const CircleAvatar(
+                      radius: 30,
+                      backgroundColor: Colors.white,
+                      child: Icon(Icons.person, size: 40, color: Colors.black),
+                    ),
+                    const SizedBox(height: 10),
+                    const Text(
+                      "Sakay Administrative",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    DrawerItem(icon: Icons.map, text: "Surveillance"),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.pushReplacementNamed(context, '/map');
+                      },
+                      child: DrawerItem(icon: Icons.map, text: "Map"),
+                    ),
+                    DrawerItem(icon: Icons.bar_chart, text: "Report"),
+                    DrawerItem(
+                        icon: Icons.notifications, text: "Notifications"),
+                    DrawerItem(icon: Icons.inbox, text: "Inbox"),
+                    DrawerItem(icon: Icons.settings, text: "Settings"),
+                  ],
+                ),
+              ),
+              const Spacer(),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: DrawerItem(icon: Icons.logout, text: "Logout"),
+              ),
+              const SizedBox(height: 20),
+            ],
+          ),
+        ),
+      ),
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.grey[300],
+              borderRadius: BorderRadius.circular(16.0),
+              border: Border.all(color: Colors.grey),
+            ),
+            child: ClipRRect(
+              child: Center(
+                child: MyMapWidget(),
+              ),
+            ),
+          ),
+
+          Positioned(
+            top: 40,
+            left: 10,
+            child: Builder(
+              builder: (context) {
+                return IconButton(
+                  icon: const Icon(Icons.menu, color: Colors.white, size: 50),
+                  onPressed: () {
+                    Scaffold.of(context).openDrawer();
+                  },
+                );
+              },
+            ),
+          ),
+
+          Positioned(
+            top: 50,
+            left: 70,
+            right: 20,
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.2),
+                    blurRadius: 5,
+                    offset: Offset(0, 2),
+                  )
+                ],
+              ),
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Search...',
+                  border: InputBorder.none,
+                  prefixIcon: Icon(Icons.search, color: Colors.grey),
+                  contentPadding:
+                      EdgeInsets.symmetric(vertical: 15, horizontal: 8),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class DrawerItem extends StatelessWidget {
+  final IconData icon;
+  final String text;
+
+  const DrawerItem({super.key, required this.icon, required this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 16),
+      child: Row(
+        children: [
+          Icon(icon, color: Colors.white),
+          const SizedBox(width: 30, height: 10),
+          Text(
+            text,
+            style: const TextStyle(color: Colors.white, fontSize: 14),
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ client/lib/presentation/screens/admin/surveillance.dart
@@ -0,0 +1,315 @@
+import 'package:flutter/material.dart';
+import 'package:sakay_app/presentation/screens/admin/map.dart'; // Import the Map widget
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const Surveillance(),
+      routes: {
+        '/map': (context) => Map(), // Navigate to the Map screen
+      },
+    );
+  }
+}
+
+class Surveillance extends StatefulWidget {
+  const Surveillance({super.key});
+
+  @override
+  _SurveillanceState createState() => _SurveillanceState();
+}
+
+class _SurveillanceState extends State<Surveillance> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: _scaffoldKey,
+      backgroundColor: Colors.white,
+      drawer: Drawer(
+        child: Container(
+          color: const Color(0xFF00A3FF),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 50),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const CircleAvatar(
+                      radius: 30,
+                      backgroundColor: Colors.white,
+                      child: Icon(Icons.person, size: 40, color: Colors.black),
+                    ),
+                    const SizedBox(height: 10),
+                    const Text(
+                      "Sakay Administrative",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    DrawerItem(icon: Icons.map, text: "Surveillance"),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.pushReplacementNamed(context, '/map'); // Navigate and replace the current screen
+                      },
+                      child: DrawerItem(icon: Icons.location_on, text: "Map"),
+                    ),
+                    DrawerItem(icon: Icons.bar_chart, text: "Report"),
+                    DrawerItem(icon: Icons.notifications, text: "Notification"),
+                    DrawerItem(icon: Icons.inbox, text: "Inbox"),
+                    DrawerItem(icon: Icons.settings, text: "Settings"),
+                  ],
+                ),
+              ),
+              const Spacer(),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: DrawerItem(icon: Icons.logout, text: "Log out"),
+              ),
+              const SizedBox(height: 20),
+            ],
+          ),
+        ),
+      ),
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        leading: IconButton(
+          icon: const Icon(Icons.menu, color: Colors.black),
+          onPressed: () {
+            _scaffoldKey.currentState?.openDrawer();
+          },
+        ),
+        title: Container(
+          decoration: BoxDecoration(
+            color: Colors.grey[200],
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: const TextField(
+            decoration: InputDecoration(
+              hintText: "Search",
+              prefixIcon: Icon(Icons.search, color: Colors.grey),
+              border: InputBorder.none,
+              contentPadding: EdgeInsets.all(15),
+            ),
+          ),
+        ),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 16.0, left: 16.0),
+            child: Row(children: [
+              const Text("Units",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  )),
+            ]),
+          ),
+          Expanded(
+            child: ListView(
+              children: [
+                UnitCard(
+                  unitName: "UNIT-17A",
+                  address:
+                      "Arellano St, Downtown District, Dagupan, 2400 Pangasinan",
+                  distance: "2 km",
+                  duration: "2h 46min",
+                ),
+                UnitCard(
+                  unitName: "UNIT-07U",
+                  address:
+                      "Arellano St, Downtown District, Dagupan, 2400 Pangasinan",
+                  distance: "2 km",
+                  duration: "2h 46min",
+                ),
+                UnitCard(
+                  unitName: "UNIT-84C",
+                  address:
+                      "Arellano St, Downtown District, Dagupan, 2400 Pangasinan",
+                  distance: "2 km",
+                  duration: "2h 46min",
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class UnitCard extends StatelessWidget {
+  final String unitName;
+  final String address;
+  final String distance;
+  final String duration;
+
+  const UnitCard({
+    super.key,
+    required this.unitName,
+    required this.address,
+    required this.distance,
+    required this.duration,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/bus.png',
+              width: 50,
+              height: 50,
+            ),
+            const SizedBox(width: 16),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    unitName,
+                    style: const TextStyle(
+                        fontSize: 12, fontWeight: FontWeight.bold),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(8),
+                      border: Border.all(
+                        color: Colors.grey[300]!,
+
+                        width: 1.0,
+                      ),
+                    ),
+                    padding: const EdgeInsets.all(8.0),
+                    margin: const EdgeInsets.only(top: 4.0),
+                    child: Text(
+                      address,
+                      style: TextStyle(
+                        fontSize: 11,
+                        color: Colors.grey[600],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(width: 10),
+            Padding(
+              padding: const EdgeInsets.only(top: 20.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Row(
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Colors.blue,
+                          borderRadius: BorderRadius.circular(3),
+                        ),
+                        padding: const EdgeInsets.all(5.0),
+                        child: const Icon(
+                          Icons.local_parking,
+                          color: Colors.white,
+                          size: 15.0,
+                        ),
+                      ),
+                      const SizedBox(width: 8),
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Colors.grey[300],
+                          borderRadius: BorderRadius.circular(3),
+                        ),
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 13.0, vertical: 3.0),
+                        child: Text(
+                          distance,
+                          style: TextStyle(
+                            fontSize: 13,
+                            color: Colors.grey[600],
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                  const SizedBox(height: 8),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.grey[300],
+                      borderRadius: BorderRadius.circular(3),
+                    ),
+                    padding: const EdgeInsets.all(5.0),
+                    child: Row(
+                      children: [
+                        const Icon(
+                          Icons.access_time,
+                          color: Colors.lightBlue,
+                          size: 15.0,
+                        ),
+                        const SizedBox(width: 8),
+                        Text(
+                          duration,
+                          style: TextStyle(
+                            fontSize: 13,
+                            color: Colors.grey[600],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class DrawerItem extends StatelessWidget {
+  final IconData icon;
+  final String text;
+
+  const DrawerItem({super.key, required this.icon, required this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 16),
+      child: Row(
+        children: [
+          Icon(icon, color: Colors.white),
+          const SizedBox(width: 30, height: 10),
+          Text(
+            text,
+            style: const TextStyle(color: Colors.white, fontSize: 14),
+          ),
+        ],
+      ),
+    );
+  }
+}
