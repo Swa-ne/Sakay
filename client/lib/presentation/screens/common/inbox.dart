9295a99eb974de7527b29a916c9e0b1325fd014e
Swa-ne <spaulbautista@gmail.com>
2025-02-27 00:36:28 +0800

    Refactored InboxScreen to receive props for better reusability and removed unnecessary state management.

--- client/lib/presentation/screens/common/inbox.dart
+++ client/lib/presentation/screens/common/inbox.dart
@@ -5,10 +5,24 @@ import 'package:sakay_app/bloc/chat/chat_event.dart';
 import 'package:sakay_app/bloc/chat/chat_state.dart';
 import 'package:sakay_app/common/widgets/chat_bubble.dart';
 import 'package:sakay_app/data/models/message.dart';
-import 'package:sakay_app/data/sources/authentication/token_controller_impl.dart';
 
+// TODO: add refresh callback
 class InboxScreen extends StatefulWidget {
-  const InboxScreen({super.key});
+  final List<MessageModel> messages;
+  final ScrollController scrollInboxController;
+  final bool is_loading_inbox;
+  final String chat_id;
+  final String user_id;
+  final void Function(MessageModel) onMessageSent;
+  const InboxScreen({
+    super.key,
+    required this.messages,
+    required this.scrollInboxController,
+    required this.is_loading_inbox,
+    required this.chat_id,
+    required this.user_id,
+    required this.onMessageSent,
+  });
 
   @override
   State<InboxScreen> createState() => _InboxScreenState();
@@ -18,24 +32,11 @@ class _InboxScreenState extends State<InboxScreen> {
   late ChatBloc _chatBloc;
 
   final TextEditingController messageController = TextEditingController();
-  final ScrollController _scrollController = ScrollController();
-  final TokenControllerImpl _tokenController = TokenControllerImpl();
-
-  late String user_id;
-  late String chat_id;
-
-  List<MessageModel> messages = [];
-
-  int currentPage = 1;
-  bool isLoading = false;
 
   @override
   void initState() {
     super.initState();
     _chatBloc = BlocProvider.of<ChatBloc>(context);
-    _chatBloc.add(CreateInboxEvent());
-    _initializeUserID();
-    _scrollController.addListener(_onScroll);
   }
 
   void sendMessage() {
@@ -44,25 +45,11 @@ class _InboxScreenState extends State<InboxScreen> {
       _chatBloc.add(
         SaveMessageEvent(
           message,
-          chat_id,
+          widget.chat_id,
           "",
         ),
       );
     }
-  }
-
-  void _onScroll() {
-    if (_scrollController.position.pixels <= 100 && !isLoading) {
-      setState(() {
-        isLoading = true;
-        currentPage++;
-      });
-      _chatBloc.add(GetMessageEvent(chat_id, currentPage));
-    }
-  }
-
-  Future<void> _initializeUserID() async {
-    user_id = await _tokenController.getUserID();
   }
 
   @override
@@ -70,49 +57,27 @@ class _InboxScreenState extends State<InboxScreen> {
     return BlocListener<ChatBloc, ChatState>(
       listener: (context, state) {
         if (state is SaveMessageSuccess) {
+          widget.onMessageSent(
+            MessageModel(
+              message: messageController.text,
+              sender: widget.user_id,
+              chat_id: widget.chat_id,
+              is_read: false,
+              created_at: DateTime.now().toString(),
+              updated_at: DateTime.now().toString(),
+            ),
+          );
           setState(() {
-            messages.insert(
-              0,
-              MessageModel(
-                message: messageController.text,
-                sender: user_id,
-                chat_id: chat_id,
-                is_read: false,
-                created_at: DateTime.now().toString(),
-                updated_at: DateTime.now().toString(),
-              ),
-            );
             messageController.clear();
           });
-        } else if (state is CreateInboxSuccess) {
-          setState(() {
-            chat_id = state.chat_id;
-          });
-
-          _chatBloc.add(GetMessageEvent(chat_id, currentPage));
-        } else if (state is GetMessageSuccess) {
-          setState(() {
-            messages.addAll(state.messages);
-            isLoading = false;
-          });
-        } else if (state is OnReceiveMessageSuccess) {
-          if (chat_id == state.message.chat_id) {
-            setState(() {
-              messages.insert(0, state.message);
-            });
-          }
         }
       },
       child: Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.white,
           elevation: 1,
-          leading: IconButton(
-            icon: const Icon(Icons.arrow_back,
-                color: Colors.black), // back button
-            onPressed: () => Navigator.pop(context),
-          ),
           title: const Row(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               CircleAvatar(
                 backgroundImage: AssetImage("assets/bus.png"),
@@ -126,12 +91,12 @@ class _InboxScreenState extends State<InboxScreen> {
           children: [
             Expanded(
               child: ListView.builder(
-                itemCount: messages.length,
+                itemCount: widget.messages.length,
                 reverse: true,
-                controller: _scrollController,
+                controller: widget.scrollInboxController,
                 itemBuilder: (context, index) {
-                  bool isMe = user_id == messages[index].sender;
-                  return chatBubble(messages[index].message, isMe);
+                  bool isMe = widget.user_id == widget.messages[index].sender;
+                  return chatBubble(widget.messages[index].message, isMe);
                 },
               ),
             ),
