d5594d82df4d2889af33301bfbe41b548bb26d57
Stephen Bautista <68417529+Swa-ne@users.noreply.github.com>
2025-02-27 22:37:36 +0800

    Update app.py

--- api/python/app.py
+++ api/python/app.py
@@ -184,4 +184,5 @@ def index():
     return "Hello guys"
 
 if __name__ == "_main_":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
