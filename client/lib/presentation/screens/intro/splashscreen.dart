b78d78b0260f18936cd74a3aa5dc91f300539f6a
Swa-ne <spaulbautista@gmail.com>
2025-03-03 18:11:45 +0800

    Add internet connectivity check and offline authentication fallback in SplashScreen.

--- client/lib/presentation/screens/intro/splashscreen.dart
+++ client/lib/presentation/screens/intro/splashscreen.dart
@@ -1,9 +1,11 @@
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:sakay_app/bloc/authentication/auth_bloc.dart';
 import 'package:sakay_app/bloc/authentication/auth_event.dart';
 import 'package:sakay_app/bloc/authentication/auth_state.dart';
+import 'package:sakay_app/data/sources/authentication/token_controller_impl.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -13,21 +15,45 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  final TokenControllerImpl _tokenController = TokenControllerImpl();
   @override
   void initState() {
     super.initState();
+    _checkInternetAndAuthenticate();
+  }
 
+  Future<void> _checkInternetAndAuthenticate() async {
     Future.any([
-      Future.delayed(const Duration(seconds: 3)), // Timeout
+      Future.delayed(const Duration(seconds: 3)),
       _authenticateUser(),
-    ]).then((_) {
+    ]).then((_) async {
       if (mounted) {
         final state = context.read<AuthBloc>().state;
+
         if (state is AuthSuccess) {
           if (state.userType == "ADMIN") {
             context.go('/admin/home');
             return;
           } else if (state.userType == "DRIVER") {
+            context.go('/driver/home');
+            return;
+          }
+          context.go('/commuter/home');
+        } else if (state is AuthFailure &&
+            state.error == "No internet connection") {
+          final List<ConnectivityResult> connectivityResult =
+              await Connectivity().checkConnectivity();
+          if (connectivityResult.contains(ConnectivityResult.none)) {
+            if (mounted) {
+              context.go('/no_internet_connection');
+            }
+            return;
+          }
+          final String user_type = await _tokenController.getUserType();
+          if (user_type == "ADMIN") {
+            context.go('/admin/home');
+            return;
+          } else if (user_type == "DRIVER") {
             context.go('/driver/home');
             return;
           }
@@ -41,8 +67,7 @@ class _SplashScreenState extends State<SplashScreen> {
 
   Future<void> _authenticateUser() async {
     context.read<AuthBloc>().add(AuthenticateTokenEvent());
-    await Future.delayed(
-        const Duration(seconds: 3)); // Ensure splash time is at least 3s
+    await Future.delayed(const Duration(seconds: 3));
   }
 
   @override
