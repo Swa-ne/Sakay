704366e959d2c7060868392e6aa31f9fef0fbc62
Swa-ne <spaulbautista@gmail.com>
2025-03-06 20:12:20 +0800

    Refactor admin inbox with cached images and improved message read state handling.

--- client/lib/presentation/screens/admin/admin_inbox.dart
+++ client/lib/presentation/screens/admin/admin_inbox.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:sakay_app/bloc/chat/chat_bloc.dart';
@@ -6,6 +7,7 @@ import 'package:sakay_app/bloc/chat/chat_state.dart';
 import 'package:sakay_app/common/mixins/convertion.dart';
 import 'package:sakay_app/data/models/inbox.dart';
 import 'package:sakay_app/data/models/message.dart';
+import 'package:sakay_app/data/sources/authentication/token_controller_impl.dart';
 import 'package:sakay_app/presentation/screens/admin/admin_chat_page.dart';
 
 class AdminInbox extends StatefulWidget {
@@ -18,11 +20,14 @@ class AdminInbox extends StatefulWidget {
 }
 
 class _AdminInboxState extends State<AdminInbox> with Convertion {
+  final TokenControllerImpl _tokenController = TokenControllerImpl();
   final List<InboxModel> inboxes = [];
   late ChatBloc _chatBloc;
   final ScrollController _scrollController = ScrollController();
   int currentPage = 1;
   bool isLoading = false;
+
+  late String user_id;
 
   @override
   void initState() {
@@ -30,6 +35,11 @@ class _AdminInboxState extends State<AdminInbox> with Convertion {
     _chatBloc = BlocProvider.of<ChatBloc>(context);
     _chatBloc.add(GetInboxesEvent(currentPage));
     _scrollController.addListener(_onScroll);
+    _initializeMyUserModel();
+  }
+
+  Future<void> _initializeMyUserModel() async {
+    user_id = await _tokenController.getUserID();
   }
 
   void _updateInboxList(MessageModel message) {
@@ -78,29 +88,42 @@ class _AdminInboxState extends State<AdminInbox> with Convertion {
             itemCount: inboxes.length,
             itemBuilder: (context, index) {
               final inbox = inboxes[index];
+              final is_read = inbox.last_message.sender == user_id
+                  ? true
+                  : inbox.last_message.is_read;
               return ListTile(
-                leading: CircleAvatar(
-                  radius: 28,
-                  backgroundImage: NetworkImage(inbox.user_id.profile),
+                leading: CachedNetworkImage(
+                  imageUrl: inbox.user_id.profile,
+                  imageBuilder: (context, imageProvider) => CircleAvatar(
+                    radius: 28,
+                    backgroundImage: imageProvider,
+                  ),
+                  placeholder: (context, url) => const CircleAvatar(
+                    radius: 28,
+                    child: Icon(Icons.person),
+                  ),
+                  errorWidget: (context, url, error) => const CircleAvatar(
+                    radius: 28,
+                    child: Icon(Icons.error),
+                  ),
                 ),
                 title: Text(
                   "${inbox.user_id.first_name} ${inbox.user_id.last_name}",
                   style: TextStyle(
                     fontSize: 16,
                     fontWeight: FontWeight.bold,
-                    color:
-                        inbox.last_message.is_read ? Colors.blue : Colors.black,
+                    color: !is_read ? Colors.blue : Colors.black,
                   ),
                 ),
                 subtitle: Text(
-                  inbox.last_message.message,
+                  inbox.last_message.sender == user_id
+                      ? "You: ${inbox.last_message.message}"
+                      : inbox.last_message.message,
                   maxLines: 1,
                   overflow: TextOverflow.ellipsis,
                   style: TextStyle(
-                    fontWeight: inbox.last_message.is_read
-                        ? FontWeight.bold
-                        : FontWeight.normal,
-                    color: Colors.grey,
+                    fontWeight: !is_read ? FontWeight.bold : FontWeight.normal,
+                    color: !is_read ? Colors.black : Colors.grey,
                   ),
                 ),
                 trailing: Row(
@@ -112,7 +135,7 @@ class _AdminInboxState extends State<AdminInbox> with Convertion {
                       ),
                       style: const TextStyle(color: Colors.grey, fontSize: 12),
                     ),
-                    if (inbox.last_message.is_read)
+                    if (!is_read)
                       const Padding(
                         padding: EdgeInsets.only(left: 8.0),
                         child: Icon(Icons.circle, color: Colors.blue, size: 10),
@@ -144,61 +167,86 @@ class _AdminInboxState extends State<AdminInbox> with Convertion {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        elevation: 0,
-        automaticallyImplyLeading: false,
-        title: Row(
-          children: [
-            IconButton(
-              icon: const Icon(Icons.menu, color: Colors.black, size: 25),
-              onPressed: widget.openDrawer,
-            ),
-            const Text(
-              "Inbox",
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-                color: Colors.black,
-              ),
-            ),
-          ],
-        ),
-      ),
-      body: BlocBuilder<ChatBloc, ChatState>(
-        builder: (context, state) {
-          if (state is ChatLoading) {
-            return const Center(child: CircularProgressIndicator());
-          } else if (state is GetInboxesSuccess) {
-            inboxes.clear();
-            inboxes.addAll(state.inboxes);
-          } else if (state is OnReceiveMessageSuccess) {
-            final index = inboxes
-                .indexWhere((inbox) => inbox.id == state.message.chat_id);
+    return BlocListener<ChatBloc, ChatState>(
+      listener: (context, state) {
+        if (state is ChatLoading) {
+        } else if (state is IsReadInboxSuccess) {
+          setState(() {
+            final index =
+                inboxes.indexWhere((inbox) => inbox.id == state.chat_id);
             if (index != -1) {
-              final updatedInbox =
-                  inboxes.removeAt(index).copyWith(last_message: state.message);
-              inboxes.insert(0, updatedInbox);
-            } else {
-              inboxes.insert(
-                0,
-                InboxModel(
-                  id: state.message.chat_id,
-                  user_id: state.user,
-                  last_message: state.message,
-                  is_active: true,
-                ),
+              final updatedInbox = inboxes[index].copyWith(
+                last_message:
+                    inboxes[index].last_message.copyWith(is_read: true),
               );
+              inboxes[index] = updatedInbox;
             }
-          } else if (state is GetInboxesError) {
-            return Center(child: Text("Error: ${state.error}"));
-          }
-          return inboxes.isEmpty
-              ? const Center(child: Text("No messages available"))
-              : _buildInboxList();
-        },
+          });
+        } else if (state is GetInboxesSuccess) {
+          setState(() {
+            isLoading = false;
+            inboxes.addAll(state.inboxes);
+          });
+        } else if (state is IsReadInboxError) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text(state.error)),
+          );
+        }
+      },
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          elevation: 0,
+          automaticallyImplyLeading: false,
+          title: Row(
+            children: [
+              IconButton(
+                icon: const Icon(Icons.menu, color: Colors.black, size: 25),
+                onPressed: widget.openDrawer,
+              ),
+              const Text(
+                "Inbox",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              ),
+            ],
+          ),
+        ),
+        body: BlocBuilder<ChatBloc, ChatState>(
+          builder: (context, state) {
+            if (state is ChatLoading) {
+              return const Center(child: CircularProgressIndicator());
+            } else if (state is OnReceiveMessageSuccess) {
+              final index = inboxes
+                  .indexWhere((inbox) => inbox.id == state.message.chat_id);
+              if (index != -1) {
+                final updatedInbox = inboxes
+                    .removeAt(index)
+                    .copyWith(last_message: state.message);
+                inboxes.insert(0, updatedInbox);
+              } else {
+                inboxes.insert(
+                  0,
+                  InboxModel(
+                    id: state.message.chat_id,
+                    user_id: state.user,
+                    last_message: state.message,
+                    is_active: true,
+                  ),
+                );
+              }
+            } else if (state is GetInboxesError) {
+              return Center(child: Text("Error: ${state.error}"));
+            }
+            return inboxes.isEmpty
+                ? const Center(child: Text("No messages available"))
+                : _buildInboxList();
+          },
+        ),
       ),
     );
   }
