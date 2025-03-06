7fa7302f4041a4719fb7c811ec7346649189cbb9
Swa-ne <spaulbautista@gmail.com>
2025-03-06 20:00:33 +0800

    Added formatToDate, formatToTime, timeAgo, and formatTimeOfDay methods in Convertion Mixin.

--- client/lib/common/mixins/convertion.dart
+++ client/lib/common/mixins/convertion.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 mixin Convertion {
@@ -14,6 +15,18 @@ mixin Convertion {
     }
   }
 
+  String formatToDate(String dtString) {
+    DateTime dt = DateTime.parse(dtString).toLocal();
+
+    return DateFormat('MMM d, y').format(dt); // Format: Jan 13, 2025, 11:00 PM
+  }
+
+  String formatToTime(String dtString) {
+    DateTime dt = DateTime.parse(dtString).toLocal();
+
+    return DateFormat('h:mm a').format(dt); // Format: Jan 13, 2025, 11:00 PM
+  }
+
   String formatDate(String timestamp) {
     DateTime dateTime = DateTime.parse(timestamp);
     DateTime now = DateTime.now();
@@ -25,5 +38,37 @@ mixin Convertion {
     } else {
       return "${dateTime.month}/${dateTime.day}/${dateTime.year}, ${dateTime.hour}:${dateTime.minute.toString().padLeft(2, '0')}";
     }
+  }
+
+  String timeAgo(String dtString) {
+    DateTime dt = DateTime.parse(dtString).toLocal();
+    Duration diff = DateTime.now().difference(dt);
+
+    if (diff.inMinutes < 1) {
+      return "Just now";
+    } else if (diff.inMinutes < 60) {
+      return "${diff.inMinutes} minute${diff.inMinutes > 1 ? 's' : ''} ago";
+    } else if (diff.inHours < 24) {
+      return "${diff.inHours} hour${diff.inHours > 1 ? 's' : ''} ago";
+    } else if (diff.inDays < 7) {
+      return "${diff.inDays} day${diff.inDays > 1 ? 's' : ''} ago";
+    } else if (diff.inDays < 30) {
+      int weeks = (diff.inDays / 7).floor();
+      return "${weeks} week${weeks > 1 ? 's' : ''} ago";
+    } else if (diff.inDays < 365) {
+      int months = (diff.inDays / 30).floor();
+      return "${months} month${months > 1 ? 's' : ''} ago";
+    } else {
+      int years = (diff.inDays / 365).floor();
+      return "${years} year${years > 1 ? 's' : ''} ago";
+    }
+  }
+
+  String formatTimeOfDay(TimeOfDay time) {
+    String hour = time.hour.toString().padLeft(2, '0');
+    String minute = time.minute.toString().padLeft(2, '0');
+    String period = time.hour < 12 ? 'AM' : 'PM';
+
+    return '$hour:$minute$period';
   }
 }
