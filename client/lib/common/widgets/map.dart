1b686ce38dfe3395e8086f5a77efe72f95f0091c
Swa-ne <spaulbautista@gmail.com>
2025-03-22 01:47:38 +0800

    Migrate Mapbox to Google Maps.

--- client/lib/common/widgets/map.dart
+++ client/lib/common/widgets/map.dart
@@ -1,5 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
-import 'package:mapbox_maps_flutter/mapbox_maps_flutter.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:sakay_app/common/mixins/tracker.dart';
 
 class MyMapWidget extends StatefulWidget {
@@ -9,12 +11,11 @@ class MyMapWidget extends StatefulWidget {
   _MyMapScreenState createState() => _MyMapScreenState();
 }
 
-class _MyMapScreenState extends State<MyMapWidget> with Tracker {
-  _onMapCreated(MapboxMap mapboxMap) {
-    setState(() {
-      setMap(mapboxMap);
-    });
-  }
+class _MyMapScreenState extends State<MyMapWidget> {
+  final Completer<GoogleMapController> _mapController = Completer();
+  final Tracker tracker = Tracker();
+
+  bool trackMe = false;
 
   @override
   void initState() {
@@ -28,18 +29,51 @@ class _MyMapScreenState extends State<MyMapWidget> with Tracker {
 
   @override
   Widget build(BuildContext context) {
-    final MapWidget mapWidget = MapWidget(
-      key: const ValueKey("mapWidget"),
-      styleUri: MapboxStyles.STANDARD,
-      onMapCreated: _onMapCreated,
-      cameraOptions: CameraOptions(
-        center: Point(coordinates: Position(120.286585, 16.033410)),
-        zoom: 11.0,
-      ),
-    );
-
+    MapType _currentMapType = MapType.normal;
     return SizedBox.expand(
-      child: mapWidget,
+      child: ValueListenableBuilder<Set<Marker>>(
+        valueListenable: tracker.markersNotifier,
+        builder: (context, markers, _) {
+          return ValueListenableBuilder<bool>(
+            valueListenable: tracker.showMyLocationBool,
+            builder: (context, showMyLocation, _) {
+              return ValueListenableBuilder<Set<Polyline>>(
+                valueListenable: tracker.polylineNotifier,
+                builder: (context, polylines, child) {
+                  return GoogleMap(
+                    initialCameraPosition: const CameraPosition(
+                      target: LatLng(16.033410, 120.286585),
+                      zoom: 12.0,
+                    ),
+                    mapType: _currentMapType,
+                    onMapCreated: (GoogleMapController controller) async {
+                      controller.setMapStyle('''[
+                          {
+                            "featureType": "poi",
+                            "elementType": "all",
+                            "stylers": [
+                              { "visibility": "off" }
+                            ]
+                          }
+                        ]''');
+                      setState(() {
+                        tracker.setMap(controller);
+                      });
+                      _mapController.complete(controller);
+                    },
+                    mapToolbarEnabled: false,
+                    zoomControlsEnabled: false,
+                    myLocationEnabled: showMyLocation,
+                    myLocationButtonEnabled: false,
+                    markers: markers,
+                    polylines: polylines,
+                  );
+                },
+              );
+            },
+          );
+        },
+      ),
     );
   }
 }
