48b67ead90e6f2e41fb93d443bfc0d9ea15fb709
Swa-ne <spaulbautista@gmail.com>
2025-05-31 23:36:43 +0800

    Created report Icons

---
+++ web/src/components/icons/incidentReport.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const IncidentReport = () => {
+    return (
+        <svg className='inline' xmlns='http://www.w3.org/2000/svg' width='15' height='15' viewBox='0 0 15 15' fill='none'>
+            <circle cx='7.5' cy='7.5' r='7.5' fill='#BB0003' />
+            <path d='M7.70937 7.83984V6.16016H6.88906V7.83984H7.70937ZM7.70937 9.5V8.66016H6.88906V9.5H7.70937ZM2.70937 10.75L7.29922 2.83984L11.8891 10.75H2.70937Z' fill='white' />
+        </svg>
+    );
+};
+
+export default IncidentReport;
