0adab4fd1789e8c8650e1d168780f16500f27564
Swa-ne <spaulbautista@gmail.com>
2025-06-26 23:54:40 +0800

    Create useAuthenticated hook to manage user authentication status and user type

---
+++ web/src/hooks/useAuthenticated.ts
@@ -0,0 +1,29 @@
+import { useEffect, useState } from 'react'
+import { authenticateToken } from '@/service/auth'
+
+interface AuthStatus {
+    isAuthenticated: boolean | null
+    userType: string | null
+}
+
+export const useAuthenticated = (): AuthStatus => {
+    const [isAuthenticated, setIsAuthenticated] = useState<boolean | null>(null)
+    const [userType, setUserType] = useState<string | null>(null)
+
+    useEffect(() => {
+        const checkAuth = async () => {
+            const result = await authenticateToken()
+            if (typeof result === 'object' && result.is_authenticated) {
+                setIsAuthenticated(true)
+                setUserType(result.user_type)
+            } else {
+                setIsAuthenticated(false)
+                setUserType(null)
+            }
+        }
+
+        checkAuth()
+    }, [])
+
+    return { isAuthenticated, userType }
+}
