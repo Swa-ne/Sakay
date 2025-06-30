6155b70b6716ec019c15e3a43068fb886e5d30b1
Swa-ne <spaulbautista@gmail.com>
2025-06-30 10:57:08 +0800

    Added service function for creating driver account.

--- web/src/service/users.ts
+++ web/src/service/users.ts
@@ -1,6 +1,8 @@
 import { useAuthStore } from "@/stores";
 import api from ".";
 import { AxiosError } from "axios";
+import { UserModel } from "@/schema/account.unit.schema";
+import { Account } from "@/types";
 
 const ROUTE = "/user"
 export const getAllUsers = async (page: number) => {
@@ -31,6 +33,33 @@ export const getUser = async (user_id: string) => {
 
         const data = response.data;
         return data.message;
+    } catch (error: unknown) {
+        const axiosError = error as AxiosError<{ error: string }>;
+        const errMsg = axiosError.response?.data?.error || 'Unknown error';
+        return errMsg;
+    }
+}
+export const postDriver = async (driver: UserModel) => {
+    const { access_token } = useAuthStore.getState();
+    try {
+        const response = await api.post(
+            `${ROUTE}/create-driver`,
+            driver,
+            {
+                headers: {
+                    "Authorization": access_token
+                }
+            }
+        );
+
+        const data = response.data;
+        const user: Account = {
+            id: data.user_id,
+            name: `${data.first_name} ${data.last_name}`,
+            role: data.user_type,
+            assignedUnitId: data.assigned_bus_id,
+        }
+        return user;
     } catch (error: unknown) {
         const axiosError = error as AxiosError<{ error: string }>;
         const errMsg = axiosError.response?.data?.error || 'Unknown error';
