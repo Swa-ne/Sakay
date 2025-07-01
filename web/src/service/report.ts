9c84a261b0ba6b6a6974f0246db052a14ff1c284
Swa-ne <spaulbautista@gmail.com>
2025-07-02 00:01:15 +0800

    Added API functions for fetching reports and report statistics.

---
+++ web/src/service/report.ts
@@ -0,0 +1,64 @@
+import { useAuthStore } from "@/stores";
+import api from ".";
+import { AxiosError } from "axios";
+
+const ROUTE = "/report"
+export const getAllReports = async (page: number) => {
+    const { access_token } = useAuthStore.getState();
+    try {
+        const response = await api.get(
+            `${ROUTE}/get-all-report/:page`,
+            {
+                headers: {
+                    "Authorization": access_token
+                },
+                params: { page }
+            }
+        );
+        const data = response.data;
+        return data.message;
+    } catch (error: unknown) {
+        const axiosError = error as AxiosError<{ error: string }>;
+        const errMsg = axiosError.response?.data?.error || 'Unknown error';
+        return errMsg;
+    }
+}
+export const getReport = async (report_id: string) => {
+    const { access_token } = useAuthStore.getState();
+    try {
+        const response = await api.get(
+            `${ROUTE}/get-report/${report_id}`,
+            {
+                headers: {
+                    "Authorization": access_token
+                }
+            }
+        );
+
+        const data = response.data;
+        return data.message;
+    } catch (error: unknown) {
+        const axiosError = error as AxiosError<{ error: string }>;
+        const errMsg = axiosError.response?.data?.error || 'Unknown error';
+        return errMsg;
+    }
+}
+export const getReportStats = async () => {
+    const { access_token } = useAuthStore.getState();
+    try {
+        const response = await api.get(
+            `${ROUTE}/get-stats-report`,
+            {
+                headers: {
+                    "Authorization": access_token
+                }
+            }
+        );
+        const data = response.data;
+        return data.message;
+    } catch (error: unknown) {
+        const axiosError = error as AxiosError<{ error: string }>;
+        const errMsg = axiosError.response?.data?.error || 'Unknown error';
+        return errMsg;
+    }
+}
