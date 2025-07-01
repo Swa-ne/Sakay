f80e73453c0dada1158f817ca89bbbd97aaee6c2
Swa-ne <spaulbautista@gmail.com>
2025-07-02 00:28:36 +0800

    Updated types to align with updated report, bus, and user data structures.

--- web/src/types/index.ts
+++ web/src/types/index.ts
@@ -1,17 +1,67 @@
 
 export type Role = 'DRIVER' | 'COMMUTER' | 'ADMIN';
 
+export interface fetchUser {
+    _id: string
+    first_name: string
+    last_name: string
+    user_type: string
+    assigned_bus_id: string
+    phone_number: string
+    profile_picture_url: string
+}
+export interface fetchBus {
+    _id: string
+    bus_number: string
+    plate_number: string
+    today_driver?: fetchUser
+}
 export interface Account {
-    id: number;
+    id: string;
     name: string;
     role: Role;
-    assignedUnitId?: number | null;
+    assignedUnitId?: string | null;
+    phone_number: string;
+    profile_picture_url: string;
 }
 
 export interface Unit {
-    id: number;
+    id: string;
     name: string;
     bus_number: string;
     plate_number: string;
-    assignedDriverId?: number | null;
+    assignedDriverId?: string | null;
+}
+
+export interface Report {
+    _id: string,
+    bus: fetchBus,
+    reporter: fetchUser,
+    investigator?: string | null,
+    driver?: fetchUser | null,
+    type_of_report: TypesOfReport,
+    description?: string | null,
+    place_of_incident?: string | null,
+    time_of_incident?: string | null,
+    date_of_incident?: string | null,
+    driving_rate?: number | null,
+    service_rate?: number | null,
+    reliability_rate?: number | null,
+    is_open: boolean,
+    createdAt?: string | null,
+    updatedAt?: string | null,
+}
+
+export type TypesOfReport = "PERFORMANCE" | "INCIDENT" | "MAINTENANCE";
+
+export interface ReportStatItem {
+    count: number;
+    change: number;
+}
+
+export interface FetchReportStats {
+    open: ReportStatItem;
+    closed: ReportStatItem;
+    assigned: ReportStatItem;
+    unassigned: ReportStatItem;
 }
