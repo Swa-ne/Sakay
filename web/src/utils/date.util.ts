1c38394f003810fac34b93fa1dcbe51838e1b61f
Swa-ne <spaulbautista@gmail.com>
2025-07-01 23:57:42 +0800

    Added timeAgo and reportDate utility functions for formatting date.

--- web/src/utils/date.util.ts
+++ web/src/utils/date.util.ts
@@ -3,3 +3,45 @@ export const getMaxDate = () => {
     const maxDate = new Date(today.getFullYear() - 18, today.getMonth(), today.getDate());
     return maxDate.toISOString().split('T')[0];
 };
+
+export const timeAgo = (dtString: string) => {
+    const dt = new Date(dtString);
+    const now = new Date();
+    const diffMs = now.getTime() - dt.getTime();
+    const diffMins = Math.floor(diffMs / (1000 * 60));
+    const diffHours = Math.floor(diffMs / (1000 * 60 * 60));
+    const diffDays = Math.floor(diffMs / (1000 * 60 * 60 * 24));
+
+    if (diffMins < 1) {
+        return "Just now";
+    } else if (diffMins < 60) {
+        return `${diffMins} minute${diffMins > 1 ? 's' : ''} ago`;
+    } else if (diffHours < 24) {
+        return `${diffHours} hour${diffHours > 1 ? 's' : ''} ago`;
+    } else if (diffDays < 7) {
+        return `${diffDays} day${diffDays > 1 ? 's' : ''} ago`;
+    } else if (diffDays < 30) {
+        const weeks = Math.floor(diffDays / 7);
+        return `${weeks} week${weeks > 1 ? 's' : ''} ago`;
+    } else if (diffDays < 365) {
+        const months = Math.floor(diffDays / 30);
+        return `${months} month${months > 1 ? 's' : ''} ago`;
+    } else {
+        const years = Math.floor(diffDays / 365);
+        return `${years} year${years > 1 ? 's' : ''} ago`;
+    }
+}
+
+export const reportDate = (rawDate: string) => {
+    const date = new Date(rawDate);
+
+    const time = date.toLocaleTimeString('en-US', {
+        hour: 'numeric',
+        minute: '2-digit',
+        hour12: true,
+    });
+
+    const formattedDate = date.toLocaleDateString('en-GB').replace(/\//g, '/');
+    return `${time} | ${formattedDate}`;
+
+}
