308f8f8f00ce4fec404162598b195db5d1478f68
Swa-ne <spaulbautista@gmail.com>
2025-07-15 11:32:33 +0800

    Add API service functions for announcement CRUD operations.

---
+++ web/src/service/announcment.ts
@@ -0,0 +1,108 @@
+import { useAuthStore } from "@/stores";
+import api from ".";
+import { AxiosError } from "axios";
+
+const ROUTE = "/announcement"
+export const getAllAnnouncements = async (page: number) => {
+    const { access_token } = useAuthStore.getState();
+    try {
+        const response = await api.get(
+            `${ROUTE}/get-all-announcements/${page}`,
+            {
+                headers: {
+                    "Authorization": access_token
+                }
+            }
+        );
+        const data = response.data;
+        return data.message;
+    } catch (error: unknown) {
+        const axiosError = error as AxiosError<{ error: string }>;
+        const errMsg = axiosError.response?.data?.error || 'Unknown error';
+        return errMsg;
+    }
+}
+export const getAnnouncement = async (announcement_id: string) => {
+    const { access_token } = useAuthStore.getState();
+    try {
+        const response = await api.get(
+            `${ROUTE}/get-announcement/${announcement_id}`,
+            {
+                headers: {
+                    "Authorization": access_token
+                }
+            }
+        );
+
+        const data = response.data;
+        return data.message;
+    } catch (error: unknown) {
+        const axiosError = error as AxiosError<{ error: string }>;
+        const errMsg = axiosError.response?.data?.error || 'Unknown error';
+        return errMsg;
+    }
+}
+export const postAnnouncement = async (formData: FormData) => {
+    const { access_token } = useAuthStore.getState();
+    try {
+        const response = await api.post(
+            `${ROUTE}/save-announcement`,
+            formData,
+            {
+                headers: {
+                    "Authorization": access_token,
+                    'Content-Type': 'multipart/form-data'
+                }
+            }
+        );
+
+        const data = response.data;
+        return data.message;
+    } catch (error: unknown) {
+        const axiosError = error as AxiosError<{ error: string }>;
+        const errMsg = axiosError.response?.data?.error || 'Unknown error';
+        return errMsg;
+    }
+}
+export const editAnnouncement = async (announcement_id: string, formData: FormData) => {
+    const { access_token } = useAuthStore.getState();
+    try {
+        const response = await api.put(
+            `${ROUTE}/edit-announcement/${announcement_id}`,
+            formData,
+            {
+                headers: {
+                    "Authorization": access_token,
+                    'Content-Type': 'multipart/form-data'
+                }
+            }
+        );
+        const data = response.data;
+        return data.message === "Success";
+    } catch (error: unknown) {
+        console.log(error)
+        const axiosError = error as AxiosError<{ error: string }>;
+        const errMsg = axiosError.response?.data?.error || 'Unknown error';
+        return errMsg;
+    }
+}
+export const deleteAnnouncement = async (announcement_id: string) => {
+    const { access_token } = useAuthStore.getState();
+    try {
+        const response = await api.delete(
+            `${ROUTE}/delete-announcement/${announcement_id}`,
+            {
+                headers: {
+                    "Authorization": access_token
+                }
+            }
+        );
+
+        const data = response.data;
+        return data.message === "Success";
+    } catch (error: unknown) {
+        const axiosError = error as AxiosError<{ error: string }>;
+        const errMsg = axiosError.response?.data?.error || 'Unknown error';
+        return errMsg;
+    }
+}
