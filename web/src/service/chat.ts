86994e5570ecb9b93db07df9de3ee52aa515d219
Swa-ne <spaulbautista@gmail.com>
2025-07-15 11:30:32 +0800

    Add chat service with message and inbox API utility functions.

---
+++ web/src/service/chat.ts
@@ -0,0 +1,125 @@
+import { AxiosError } from
+    'axios'; import api from ".";
+import { useAuthStore } from '@/stores/auth.store';
+import { Inbox, Message } from '@/types';
+import { sendMessage } from './websocket/realtime';
+const ROUTE = '/chat';
+
+export const saveMessage = async (message: string, chat_id: string, receiver_id: string) => {
+    try {
+        const { access_token } = useAuthStore.getState();
+        const response = await api.post(`${ROUTE}/save-message`, {
+            message,
+            chat_id
+        }, {
+            headers: {
+                'Authorization': access_token,
+            }
+        });
+
+        if (response.data.message === "Success") {
+            sendMessage(receiver_id, message, chat_id)
+            return true;
+        }
+        return false;
+    } catch (error) {
+        const err = error as AxiosError<{ message: string }>;
+        console.log(error)
+        const errMsg = err.response?.data?.message || 'Unknown error';
+        return errMsg;
+    }
+};
+
+export const createPrivateInbox = async (): Promise<string> => {
+    try {
+        const { access_token } = useAuthStore.getState();
+        const response = await api.post(`${ROUTE}/create-private-inbox`, {}, {
+            headers: {
+                'Authorization': access_token,
+            }
+        });
+        return response.data.message;
+    } catch (error) {
+        const err = error as AxiosError<{ error: string }>;
+        const errMsg = err.response?.data?.error || 'Unknown error';
+        return errMsg;
+    }
+};
+
+export const openCreatedInboxContentByChatID = async (chat_id: string): Promise<string> => {
+    try {
+        const { access_token } = useAuthStore.getState();
+        const response = await api.get(`${ROUTE}/open-inbox-details/${chat_id}`, {
+            headers: {
+                'Authorization': access_token,
+            }
+        });
+        return response.data.message;
+    } catch (error) {
+        const err = error as AxiosError<{ message: string }>;
+        const errMsg = err.response?.data?.message || 'Unknown error';
+        return errMsg;
+    }
+};
+
+export const getMessage = async (chat_id: string, page: number): Promise<Message[] | string> => {
+    try {
+        const { access_token } = useAuthStore.getState();
+        const response = await api.get(`${ROUTE}/get-messages/${chat_id}/${page}`, {
+            headers: {
+                'Authorization': access_token,
+            }
+        });
+        return response.data.message as Message[];
+    } catch (error) {
+        const err = error as AxiosError<{ message: string }>;
+        const errMsg = err.response?.data?.message || 'Unknown error';
+        return errMsg;
+    }
+};
+
+export const openInbox = async (): Promise<Inbox | string> => {
+    try {
+        const { access_token } = useAuthStore.getState();
+        const response = await api.get(`${ROUTE}/open-inbox`, {
+            headers: {
+                'Authorization': access_token,
+            }
+        });
+        return response.data.message as Inbox;
+    } catch (error) {
+        const err = error as AxiosError<{ message: string }>;
+        const errMsg = err.response?.data?.message || 'Unknown error';
+        return errMsg;
+    }
+};
+
+export const getAllInboxes = async (page: number): Promise<Inbox[] | string> => {
+    try {
+        const { access_token } = useAuthStore.getState();
+        const response = await api.get(`${ROUTE}/get-all-inbox/${page}`, {
+            headers: {
+                'Authorization': access_token,
+            }
+        });
+        return response.data.message as Inbox[];
+    } catch (error) {
+        const err = error as AxiosError<{ message: string }>;
+        const errMsg = err.response?.data?.message || 'Unknown error';
+        return errMsg;
+    }
+};
+
+export const isReadInboxes = async (chat_id: string): Promise<boolean> => {
+    try {
+        const { access_token } = useAuthStore.getState();
+        const response = await api.put(`${ROUTE}/is-read/${chat_id}`, {}, {
+            headers: {
+                'Authorization': access_token,
+            }
+        });
+        return response.data.message === "Success";
+    } catch {
+        return false;
+    }
+};
