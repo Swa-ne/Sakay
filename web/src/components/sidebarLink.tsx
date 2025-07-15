a86757b137a0d2164745ba7387409e4ce7ce3c86
Swa-ne <spaulbautista@gmail.com>
2025-07-15 11:00:09 +0800

    Updated link href of the sidebar links.

--- web/src/components/sidebarLink.tsx
+++ web/src/components/sidebarLink.tsx
@@ -14,7 +14,7 @@ const SidebarLink = ({ icon, hoverIcon, name, route, collapsed }: SidebarLinkPro
 
     return (
         <li className='w-full px-5 py-2'>
-            <Link href={route} className={`w-[95%] h-10 rounded-md flex items-center hover:bg-primary hover:text-background transition-all duration-200 ${collapsed ? 'justify-center' : 'justify-start px-3 space-x-4'}`} onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
+            <Link href={`/${route}`} className={`w-[95%] h-10 rounded-md flex items-center hover:bg-primary hover:text-background transition-all duration-200 ${collapsed ? 'justify-center' : 'justify-start px-3 space-x-4'}`} onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
                 <span className={`flex items-center justify-center w-7 h-7`}>{isHovered ? hoverIcon : icon}</span>
                 <label className={`cursor-pointer font-medium ${collapsed ? 'w-0 hidden' : 'w-[80%]'}`}>{name}</label>
             </Link>
