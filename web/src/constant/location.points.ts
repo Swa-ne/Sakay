8fdacfb862d998f0dae31784e0ea10151e34daf8
Swa-ne <spaulbautista@gmail.com>
2025-07-20 23:14:02 +0800

    Added allowedDestinations list with name and coordinates.

---
+++ web/src/constant/location.points.ts
@@ -0,0 +1,227 @@
+export const allowedDestinations = [
+    {
+        "name": "Caltex - Dagupan City",
+        "lat": 16.038356,
+        "lng": 120.333618,
+    },
+    {
+        "name": "University of Luzon",
+        "lat": 16.039257,
+        "lng": 120.335644,
+    },
+    {
+        "name": "Zamora Street",
+        "lat": 16.039600,
+        "lng": 120.336417,
+    },
+    {
+        "name": "Galvan Street",
+        "lat": 16.039978,
+        "lng": 120.337329,
+    },
+    {
+        "name": "St. Joseph Drug Store - Rizal Street, Dagupan City",
+        "lat": 16.040897,
+        "lng": 120.339501,
+    },
+    {
+        "name": "Pangasinan Merchant Marine Academy(PAMMA)",
+        "lat": 16.041865,
+        "lng": 120.341745,
+    },
+    {
+        "name": "Victory Liner - Dagupan City",
+        "lat": 16.042736,
+        "lng": 120.343764,
+    },
+    {
+        "name": "Development Bank of the Philippines - Dagupan City",
+        "lat": 16.043811,
+        "lng": 120.344205,
+    },
+    {
+        "name": "SM Hypermarket Dagupan",
+        "lat": 16.045222,
+        "lng": 120.343445,
+    },
+    {
+        "name": "Chowking - AB Fernandez, Dagupan City",
+        "lat": 16.045691,
+        "lng": 120.342337,
+    },
+    {
+        "name": "City Deluxe Restaurant Downtown",
+        "lat": 16.044224,
+        "lng": 120.338344,
+    },
+    {
+        "name": "Newstar Shopping Mall - Dagupan",
+        "lat": 16.044145,
+        "lng": 120.336959,
+    },
+    {
+        "name": "Dagupan City Hall",
+        "lat": 16.043928,
+        "lng": 120.335211,
+    },
+    {
+        "name": "West Central Elementary School",
+        "lat": 16.042923,
+        "lng": 120.333472,
+    },
+    {
+        "name": "Cuison Hospital Incorporated",
+        "lat": 16.040893,
+        "lng": 120.334269,
+    },
+    {
+        "name": "KOREAN PALACE (KUNG JEON RESTAURANT) - Dagupan City",
+        "lat": 16.038357,
+        "lng": 120.333202,
+    },
+    {
+        "name": "Dagupan City National High School",
+        "lat": 16.037269,
+        "lng": 120.332636,
+    },
+    {
+        "name": "Astrodome",
+        "lat": 16.036140,
+        "lng": 120.332153,
+    },
+    {
+        "name": "Lyceum-Northwestern University",
+        "lat": 16.034859,
+        "lng": 120.331570,
+    },
+    {
+        "name": "Malta",
+        "lat": 16.033534,
+        "lng": 120.330883,
+    },
+    {
+        "name": "Los Pedritos - Tapuac",
+        "lat": 16.029187,
+        "lng": 120.328743,
+    },
+    {
+        "name": "Meshroom Cafe - Tapuac",
+        "lat": 16.029187,
+        "lng": 120.328743,
+    },
+    {
+        "name": "Lavarias",
+        "lat": 16.024932,
+        "lng": 120.326576,
+    },
+    {
+        "name": "CSI Lucao",
+        "lat": 16.021238,
+        "lng": 120.324135,
+    },
+    {
+        "name": "Angels Pizza - Lucao",
+        "lat": 16.020270,
+        "lng": 120.322248,
+    },
+    {
+        "name": "Dunkin' - Lucao",
+        "lat": 16.019067,
+        "lng": 120.319937,
+    },
+    {
+        "name": "Prime Brilliant Minds Academy",
+        "lat": 16.016556,
+        "lng": 120.314625,
+    },
+    {
+        "name": "Marker Binmaley (Boundary)",
+        "lat": 16.015575,
+        "lng": 120.312421,
+    },
+    {
+        "name": "Rufina Square",
+        "lat": 16.014550,
+        "lng": 120.309931,
+    },
+    {
+        "name": "San Jose, Binmaley",
+        "lat": 16.011962,
+        "lng": 120.303920,
+    },
+    {
+        "name": "Naguilayan, Binmaley",
+        "lat": 16.010028,
+        "lng": 120.299344,
+    },
+    {
+        "name": "Centrum - Naguilayan, Binmaley",
+        "lat": 16.009052,
+        "lng": 120.295036,
+    },
+    {
+        "name": "Central Pangasinan Hospital and Medical Center",
+        "lat": 16.009445,
+        "lng": 120.291720,
+    },
+    {
+        "name": "Caltex Triangle Bus Stop - Binmaley",
+        "lat": 16.010442,
+        "lng": 120.285579,
+    },
+    {
+        "name": "Manat Bridge - Binmaley",
+        "lat": 16.013035,
+        "lng": 120.282323,
+    },
+    {
+        "name": "Linoc - Binmaley",
+        "lat": 16.014260,
+        "lng": 120.281769,
+    },
+    {
+        "name": "Nansangaan - Binmaley",
+        "lat": 16.022855,
+        "lng": 120.276433,
+    },
+    {
+        "name": "Binmaley Public Market",
+        "lat": 16.027963,
+        "lng": 120.269639,
+    },
+    {
+        "name": "Binmaley Triangle (ESTASYON)",
+        "lat": 16.029056,
+        "lng": 120.268714,
+    },
+    {
+        "name": "Manaois Furniture",
+        "lat": 16.027752,
+        "lng": 120.262536,
+    },
+    {
+        "name": "Boundary Marker Lingayen",
+        "lat": 16.025179,
+        "lng": 120.253171,
+    },
+    {
+        "name": "Shell - Lingayen",
+        "lat": 16.025110,
+        "lng": 120.245181,
+    },
+    {
+        "name": "Savewise Supermarket - Lingayen",
+        "lat": 16.022626,
+        "lng": 120.236803,
+    },
+    {
+        "name": "Pangasinan State University",
+        "lat": 16.030899,
+        "lng": 120.230123,
+    },
+    {
+        "name": "Lingayen Municipal Hall",
+        "lat": 16.020103,
+        "lng": 120.230726,
+    },
+];
