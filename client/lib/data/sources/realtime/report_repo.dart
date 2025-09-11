7a0d90e1df3f90abde897407bed56792093b66ef
Swa-ne <spaulbautista@gmail.com>
2025-09-12 00:36:20 +0800

    Updated ReportRepo to use cursor-based pagination for getAllReports.

--- client/lib/data/sources/realtime/report_repo.dart
+++ client/lib/data/sources/realtime/report_repo.dart
@@ -3,7 +3,7 @@ import 'package:sakay_app/data/models/report.dart';
 abstract class ReportRepo {
   Future<bool> postIncidentReport(ReportModel report);
   Future<bool> postPerformanceReport(ReportModel report);
-  Future<List<ReportModel>> getAllReports(int page);
+  Future<Map<String, dynamic>> getAllReports(String? cursor);
   Future<ReportModel> getReport(String report_id);
   Future<bool> toggleReport(String report_id);
 }
