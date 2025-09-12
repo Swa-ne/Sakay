783dd1acc788577730f1e382f1e1874b5db7afa2
Swa-ne <spaulbautista@gmail.com>
2025-09-13 00:36:05 +0800

    Updated package.json

--- api/javascript/ecosystem.config.js
+++ api/javascript/ecosystem.config.js
@@ -3,17 +3,15 @@ module.exports = {
         {
             name: 'sakay-api-javascript',
             script: 'dist/index.js',
-            instances: 1,
+            instances: 'max',
             autorestart: true,
             watch: false,
             max_memory_restart: '500M',
             env: {
                 NODE_ENV: 'DEV',
-                PORT: 3000,
             },
             env_production: {
                 NODE_ENV: 'PROD',
-                PORT: 3000,
             },
         },
     ],
